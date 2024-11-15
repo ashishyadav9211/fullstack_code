524c8acffb0d54df4ab7566babcc2e855a6440b8
Ashishyadav9211 <ashishkumarxpt@gmail.com>
2024-11-16 01:56:50 +0530

    calculator code changes commited

---
+++ calculator/style.js
@@ -0,0 +1,24 @@
+let input = document.getElementById("inputbox");
+
+let buttons = document.querySelectorAll("button");
+let string = "";
+
+let arr = Array.from(buttons);
+arr.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log("triggerred click button" + e.target.innerHtml);
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
